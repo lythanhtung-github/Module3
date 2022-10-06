6492fe0ac6dac2158ffd104155406da550fa6e66
Lý Thanh Tùng <lythanhtung.work@gmail.com>
2022-10-06 14:09:48 +0700

    login servlet

---
+++ Servlet/simple-login/src/main/java/com/example/simplelogin/LoginServlet.java
@@ -0,0 +1,32 @@
+package com.example.simplelogin;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class LoginServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        super.doGet(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String username = req.getParameter("username");
+        String password = req.getParameter("password");
+
+        PrintWriter writer = resp.getWriter();
+        writer.println("<html>");
+
+        if ("admin".equals(username) && "admin".equals(password)) {
+            writer.println("<h1>Welcome " + username + " to website</h1>");
+        } else {
+            writer.println("<h1>Login Error</h1>");
+        }
+
+        writer.println("</html>");
+    }
+}
