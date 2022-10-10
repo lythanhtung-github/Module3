70c01cf9a08c2821c603e7d256bf8060753a0618
Lý Thanh Tùng <lythanhtung.work@gmail.com>
2022-10-10 21:49:52 +0700

    Đóng gói thư viện giải PT bậc 2

---
+++ Dependency Management-Packaging/quadratic-equation/src/Quadratic.java
@@ -0,0 +1,33 @@
+public class Quadratic {
+    public static void main(String[] args) {
+
+    }
+
+    public static void giaiPT2(float a, float b, float c) {
+        if (a == 0) {
+            if (b == 0) {
+                System.out.println("Phương trình vô nghiệm!");
+            } else {
+                System.out.println("Phương trình có một nghiệm: "
+                        + "x = " + (-c / b));
+            }
+            return;
+        }
+        float delta = b*b - 4*a*c;
+        float x1;
+        float x2;
+
+        if (delta > 0) {
+            x1 = (float) ((-b + Math.sqrt(delta)) / (2*a));
+            x2 = (float) ((-b - Math.sqrt(delta)) / (2*a));
+            System.out.println("Phương trình có 2 nghiệm là: "
+                    + "x1 = " + x1 + " và x2 = " + x2);
+        } else if (delta == 0) {
+            x1 = (-b / (2 * a));
+            System.out.println("Phương trình có nghiệm kép: "
+                    + "x1 = x2 = " + x1);
+        } else {
+            System.out.println("Phương trình vô nghiệm!");
+        }
+    }
+}
